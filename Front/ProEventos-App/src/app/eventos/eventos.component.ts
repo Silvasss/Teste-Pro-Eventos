f62145becde877af39f302fa9d819c66c1e2c522
Silva <felipe_silva55@hotmail.com>
2022-05-13 07:19:43 -0700

    Diretivas

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos : any = [];
+
+  public eventosFiltrados : any = [];
+
+  widthImg = 50;
+  marginImg = 2;
+
+  exibirImagem = false;
+
+  private _filtroLista = '';
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value
+
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase()
+
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) != -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) != -1
+    )
+  }
+
 
   constructor(private http: HttpClient) { }
 
@@ -18,10 +46,16 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get("https://localhost:5001/Evento").subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     )
+  }
 
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem
   }
 
 }
